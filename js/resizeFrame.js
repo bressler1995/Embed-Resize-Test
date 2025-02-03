02e7bcf1b7bf5400cda296f58aa91154c3d33c38
Brian Ressler <carlofabyss@hotmail.com>
2025-02-03 14:21:12 -0600

    merge canvas code with resize code

--- js/resizeFrame.js
+++ js/resizeFrame.js
@@ -587,7 +587,19 @@
 });
 //# sourceMappingURL=/sm/1c56cdd7eda14563100cbe3d56f489c5ab421fd638b173e094428f89ef87cfb4.map
 
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(() => {
+    if (window.location.pathname.search('login')) {
+      const new_link = $('<br/><br/><a href="https://nasastem.instructure.com/login/saml" title="NASA Employee Login">NASA Employee Login</a> <br/><br/><a href="https://www.nasa.gov/accessibility/" title="NASA Accessibility">NASA Accessibility</a>');
+      $('#login_forgot_password').parent().parent().append(new_link);
+    }
+  });
+  
+$(document).ready(() => {
+    const added_text = 'By accessing and using this information system, you acknowledge and consent to the following: You are accessing a U.S. Government information system, which includes: (1) this computer; (2) this computer network; (3) all computers connected to this network including end user systems; (4) all devices and storage media attached to this network or to any computer on this network; and (5) cloud and remote information services. This information system is provided for U.S. Government-authorized use only. You have no reasonable expectation of privacy regarding any communication transmitted through or data stored on this information system. At any time, and for any lawful purpose, the U.S. Government may monitor, intercept, search, and seize any communication or data transiting, stored on, or traveling to or from this information system. You are NOT authorized to process classified information on this information system. Unauthorized or improper use of this system may result in suspension or loss of access privileges, disciplinary action, and civil and/or criminal penalties.';
+    const added_html_and_text = `<div style="clear:both"><p>${added_text}</p></div>`;
+  
+    $('#login_form').append(added_html_and_text);
+
     setTimeout(function() {
         console.log("TOPS Canvas: Attempting to resize...");
         iframeResize({ 
